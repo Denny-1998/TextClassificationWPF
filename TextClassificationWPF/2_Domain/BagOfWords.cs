df35985bb77c7d0bb8f2ce0952f54fceb656888a
Denny-1998 <89900734+Denny-1998@users.noreply.github.com>
2022-09-30 11:57:45 +0200

    Projektdateien hinzufügen.

---
+++ TextClassificationWPF/2_Domain/BagOfWords.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextClassification.Domain
+{
+    public class BagOfWords
+    {
+        readonly IDictionary<string, int> bagOfWords;
+
+        public BagOfWords()
+        {
+            bagOfWords = new SortedDictionary<string, int>();
+        }
+
+        public void InsertEntry(string word)
+        {
+
+            word = word.Trim();
+            word = removeAccessChars(word);
+            if (word.Length == 0)
+            {
+                return;
+            }
+
+
+            if (bagOfWords.ContainsKey(word))
+            {
+                int count = bagOfWords[word];
+                count++;
+                bagOfWords[word] = count;
+            }
+            else
+            {
+                bagOfWords.Add(word, 1);
+            }
+        }
+
+        public ICollection<string> GetAllWordsInDictionary()
+        {
+            return bagOfWords.Keys;
+        }
+
+        public List<string> GetEntriesInDictionary()
+        {
+            List<string> entries = new List<string>();
+            foreach (string key in bagOfWords.Keys)
+            {
+                entries.Add(key + " " + bagOfWords[key]);
+            }
+            return entries;
+        }
+
+
+
+
+
+        public string removeAccessChars(string input)
+        {
+            //punctuation to remove     - "\"" does not remove " for some reason
+            string[] charsToRemove = new string[] {",","!","-",".","?","\""};
+
+
+            //loop through the list of chars to remove
+            for (int i = 0; i < charsToRemove.Length; i++)
+            {
+                //replace caracters with nothing 
+                input = input.Replace(charsToRemove[i], "");
+
+            }
+
+            return input;
+        }
+
+    }
+}
---
+++ TextClassificationWPF/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TextClassification.Controller;
+using TextClassification.Domain;
+
+namespace TextClassificationWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartLearning_Click(object sender, RoutedEventArgs e)
+        {
+            //start time
+            DateTimeOffset now = DateTimeOffset.UtcNow; //TODO wrong format??
+
+            KnowledgeBuilder nb = new KnowledgeBuilder();
+
+            // initiate the learning process
+            nb.Train();
+
+            // getting the (whole) knowledge found in ClassA and in ClassB
+            Knowledge k = nb.GetKnowledge();
+
+
+            // get a part of the knowledge - here just for debugging
+            BagOfWords bof = k.GetBagOfWords();
+
+            //end time
+            long unixTimeMilliseconds = now.ToUnixTimeMilliseconds(); //TODO might be wrong format 
+            TrainingTime.Text = unixTimeMilliseconds + " ms"; 
+
+
+
+            List<string> entries = bof.GetEntriesInDictionary();
+        }
+    }
+}
